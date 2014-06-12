7f78d0df91fcfdc0865239119c6ffb219df34413
teitei-tk <teitei.tk@gmail.com>
2014-06-12 22:29:58 +0900

    devise redirect path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #####################
+  # devise sign out path
+  #####################
+  def after_sign_out_path_for resource
+    root_path
+  end 
+ 
+  #####################
+  # devise sign in path
+  #####################
+  def after_sign_in_path_for(resource) 
+    admin_index_path
+  end
 end
